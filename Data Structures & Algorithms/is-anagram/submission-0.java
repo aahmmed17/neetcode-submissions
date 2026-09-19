b23cabfa918c610ca49dd01ec6551b454d8fb149
Aftab Ahmmed <aftabahmmed79@gmail.com>
2026-09-19 19:37:08 +0100

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+
+        return Arrays.equals(chars2, chars1);
+
+    }
+}
